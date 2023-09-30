d5b5b671d77cf9f32055bd77cc928ef9a9423f56
bennymathew <benny.mathew@student.moringaschool.com>
2023-09-30 11:32:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainSection = document.querySelector("main");
+mainSection.parentNode.removeChild(mainSection);
+
+let newHeader = document.createElement("h1");
+if (newHeader){
+    newHeader.id = 'victory';
+}
+newHeader.textContent = "YOUR-NAME is the champion";
+
